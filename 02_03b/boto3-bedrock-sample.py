42ce31b1fa747f207f14509cff16ee2b88fd3580
Lee Assam <leeassam@gmail.com>
2024-01-29 17:08:57 -0800

    Added project files

---
+++ 02_03b/boto3-bedrock-sample.py
@@ -0,0 +1,13 @@
+#Imports
+
+#Create the bedrock client
+
+#Setting the prompt
+
+#Model specification
+
+#Configuring parameters to invoke the model
+
+#Invoke the model
+
+#Parsing and displaying the output
---
+++ 02_05b/langchain-bedrock-sample.py
@@ -0,0 +1,11 @@
+#Imports
+
+#Create the bedrock client
+
+#setting model inference parameters
+
+#Create the llm
+
+#Generate the response
+
+#Display the result
